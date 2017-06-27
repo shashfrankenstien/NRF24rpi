6795440f01aa247bce44dd05b10daef41fe083a7
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-06-27 19:50:55 -0400

    retry comeon

--- send.py
+++ send.py
@@ -55,6 +55,7 @@ try:
         if radio.isAckPayloadAvailable():
             ack = []
             radio.read(ack, radio.getDynamicPayloadSize())
+            print (ack)
             print(''.join([chr(n) for n in ack if n >= 32 and n <= 126]))
         time.sleep(5)
 except:
