519c7e1b3981c73ddfa7a4098bbfe8be808b9e48
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-06-27 22:15:44 -0400

    two way

---
+++ master.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Example program to send packets to the radio link
+#
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+from lib_nrf24 import NRF24
+import time
+import spidev
+import sys
+
+
+
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 17)
+time.sleep(1)
+radio.setPayloadSize(32)
+radio.setChannel(0x60)
+
+radio.setDataRate(NRF24.BR_1MBPS)
+radio.setPALevel(NRF24.PA_MAX)
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+radio.openWritingPipe(pipes[1])
+time.sleep(1)
+# radio2.openReadingPipe(1, pipes[1])
+radio.printDetails()
+
+# radio2.startListening()
+
+try:
+    while True:
+        try:
+            buf = list(str(sys.argv[1]))
+        except:
+            buf = list('Yo yo honey singh')
+        # send a packet to receiver
+        radio.write(buf)
+        print ("Sent:"),
+        print (buf)
+        # did it return with a payload?
+        # if radio.isAckPayloadAvailable():
+        #     pl_buffer=[]
+        #     radio.read(, radio.getDynamicPayloadSize())
+        #     print ("Received back:"),
+        #     print (pl_buffer)
+        # else:
+        #     print ("Received: no payload")
+        if radio.isAckPayloadAvailable():
+            ack = []
+            radio.read(ack, radio.getDynamicPayloadSize())
+            print('slave:'),
+            print(''.join([chr(n) for n in ack if n >= 32 and n <= 126]))
+        time.sleep(5)
+except:
+    radio.end()
+
