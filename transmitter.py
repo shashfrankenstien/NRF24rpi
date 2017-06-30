b4aee9eacee337796f74776f4dfe14f618a06655
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-06-30 01:26:30 -0400

    master slave poing

--- transmitter.py
+++ transmitter.py
@@ -45,15 +45,20 @@ class NRF_Master(NRFtxrxBase):
         try:
             count = 1
             while True:
-                self.message_tracker[ID]=msg
+                
                 ack = self._send(msg)
                 if ack:
                     try:
                         ID, ACK = ack.split('|')
                     except Exception as e:
                         print(e)
-                    if ID and ID in self.message_tracker and self.message_tracker[ID]==msg and ACK == ack: 
-                        self._incrMsgId()
+                    if ID:
+                        if ID in self.message_tracker and self.message_tracker[ID]==msg and ACK == ack: 
+                            self._incrMsgId()
+                            del self.message_tracker[ID]
+                        else:
+                            self.message_tracker[ID]=msg
+
                 time.sleep(1)
                 count += 1
                 if count > n:
