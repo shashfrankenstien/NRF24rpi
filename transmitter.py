615c5084b6e32faa356ea531f9d696a4f9958fcf
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-06-30 00:22:46 -0400

    another attempt

--- transmitter.py
+++ transmitter.py
@@ -14,26 +14,33 @@ class NRF_Master(NRFtxrxBase):
         self.setup_as_writer()
         self.radio.printDetails()
 
+    def _send(self, msg):
+        try:
+            buf = list(str(sys.argv[1]))
+        except:
+            buf = list(msg)
+        # send a packet to receiver
+        buf += list(str(count))
+        self.radio.write(buf)
+        print ("Sent:"),
+        print (buf)
+
+        if self.radio.isAckPayloadAvailable():
+            ack = []
+            self.radio.read(ack, self.radio.getDynamicPayloadSize())
+            ack = ''.join([chr(n) for n in ack if n >= 32 and n <= 126])
+
+            print('slave:'),
+            print(ack)
+            return ack
+        return None
+
 
     def ping(self, n=4):
         try:
             count = 1
             while True:
-                try:
-                    buf = list(str(sys.argv[1]))
-                except:
-                    buf = list('Yo yo honey singh')
-                # send a packet to receiver
-                buf += list(str(count))
-                self.radio.write(buf)
-                print ("Sent:"),
-                print (buf)
-
-                if self.radio.isAckPayloadAvailable():
-                    ack = []
-                    self.radio.read(ack, self.radio.getDynamicPayloadSize())
-                    print('slave:'),
-                    print(''.join([chr(n) for n in ack if n >= 32 and n <= 126]))
+                self._send('PING')
                 time.sleep(5)
                 count += 1
                 if count > n:
@@ -41,6 +48,13 @@ class NRF_Master(NRFtxrxBase):
         except Exception as e:
             print(e)
             self.kill()
+            self.setup_as_writer()
+
+
+    def send(self, msg):
+        return self._send(str(msg))
+
+
 
 if __name__ == '__main__':
     t = NRF_Master()
